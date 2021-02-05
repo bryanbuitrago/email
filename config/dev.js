93dfba0efa1109e5b67f440b785fbea8a22eb44e
Bryan <bryanbuitrago86@gmail.com>
2021-02-05 18:45:39 -0500

    remove dev.js from gitignore

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js don't commit this!!!!
+module.exports = {
+    googleClientID: '455898342666-la5ilmin1endrb32tr1nf4e1rbdpf95k.apps.googleusercontent.com',
+    googleClientSecret: '5fV4viWKwRA4qOzj51dHiB6h',
+    mongoURI: 'mongodb+srv://bryan:EmIOE7jh6FT9TPJu@cluster0.ot4dl.mongodb.net/email?retryWrites=true&w=majority',
+    cookieKey: 'sklenheskletnhlsetnhlsetnhlsetnhilsjentihsjeiothsletnhls',
+    stripePublishableKey: 'pk_test_51IGAaqFVIKy2lhiSXqCCP6TIIowwZAUgniAJD3ijhwvgyMy71oVc15klbs4EeUUcJ1wazPHPvk4oSe7u9w8d25CP00mYviO6bf',
+    stripeSecretKey: 'sk_test_51IGAaqFVIKy2lhiSTf1mJJZD9XGx7rcWVDJ9S0BgRa29GC1jrx32YAddhFsylm5nYyPXtAWaY2IgjxgONWisr5I100mR0dgM6N'
+} 
